1973555ddac0315ae9e84ab5541f2ff625db2649
João Pedro <joaolopes19032003@gmail.com>
2025-08-20 18:06:03 -0300

    Teste API REST pactumJS

---
+++ teste/api/categorias.test.js
@@ -0,0 +1,44 @@
+const { spec, request } = require('pactum');
+request.setBaseUrl('http://lojaebac.ebaconline.art.br')
+let id;
+let token;
+beforeEach(async () => {
+    token = await spec()
+    .post('http://lojaebac.ebaconline.art.br/public/authUser')
+    .withJson({
+        "email": "admin@admin.com",
+        "password":'admin123'
+    })
+    .returns('data.token')
+})
+
+
+it('Deve adicionar categoria com sucesso', async () => {
+  id = await spec()
+    .post('/api/addCategory')
+    .withHeaders('authorization', token)
+    .withBody({
+         "name": "categoria"
+    })
+    .expectStatus(200)
+    .expectJson('success',true)
+    .returns('data._id')
+});
+it('Deve editar categoria com sucesso', async () => {
+    await spec()
+    .put('/api/editCategory/:id')
+    .withHeaders('authorization', token)
+    .withPathParams('id',id)
+    .withJson({"name": "categoria editada"})
+    .expectStatus(200)
+    .expectJson('success',true)
+});
+it('Deve deletar uma categoria com sucesso',async () => {
+    await spec()
+    .delete('/api/deleteCategory/:id')
+    .withHeaders('authorization', token)
+    .withPathParams('id',id)
+    .withJson({"message": "category deleted"})
+    .expectStatus(200)
+    .expectJson('success',true)
+});
---
+++ teste/api/produtos.test.js
@@ -0,0 +1,49 @@
+const { spec, request } = require('pactum');
+request.setBaseUrl('http://lojaebac.ebaconline.art.br')
+let id;
+let token;
+beforeEach(async () => {
+    token = await spec()
+    .post('http://lojaebac.ebaconline.art.br/public/authUser')
+    .withJson({
+        "email": "admin@admin.com",
+        "password":'admin123'
+    })
+    .returns('data.token')
+})
+
+
+it('Deve adicionar produto com sucesso', async () => {
+  id = await spec()
+    .post('/api/addProduct')
+    .withHeaders('authorization', token)
+    .withBody({
+         "name": "samsung",
+         "price": "1500",
+         "quantity": "2"
+    })
+    .expectStatus(200)
+    .expectJson('success',true)
+    .returns('data._id')
+});
+it('Deve editar produto com sucesso', async () => {
+    await spec()
+    .put('/api/editProduct/:id')
+    .withHeaders('authorization', token)
+    .withPathParams('id',id)
+    .withJson({
+        "name": "iphone",
+        "price": "5000",
+        "quantity": "1"
+    })
+    .expectStatus(200)
+    .expectJson('success',true)
+});
+it('Deve deletar um produto com sucesso',async () => {
+    await spec()
+    .delete('/api/deleteProduct/:id')
+    .withHeaders('authorization', token)
+    .withPathParams('id',id)
+    .expectStatus(200)
+    .expectJson('success',true)
+});
