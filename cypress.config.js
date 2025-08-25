a00618391e394f77b21a440b8e461204163bb9cf
João Pedro <joaolopes19032003@gmail.com>
2025-08-25 14:30:35 -0300

    Git Acitons

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome",
+    overwrite: false,
+    "reportFilename":"index.html",
+    html: true,
+    json: false
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      return config;
+    },
+  },
+});
